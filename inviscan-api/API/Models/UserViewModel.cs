535d1dd9ffc81a969f451cd1efb444883d474d79
Felipe Valentim <felipe@practicesoftware.com.br>
2024-12-22 18:40:47 -0300

    feat: formulario

---
+++ inviscan-api/API/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Models
+{
+	public class UserViewModel
+	{
+		[JsonPropertyName("id")]
+		public Guid Id { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+	}
+}
